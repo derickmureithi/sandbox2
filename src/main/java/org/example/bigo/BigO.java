a28edb149b66a5ffae93cafec8bf0f072515f0d0
derickmureithi <der2143326@gmail.com>
2023-04-18 16:23:36 -0700

    adding comparable examples

---
+++ src/main/java/org/example/bigo/BigO.java
@@ -0,0 +1,4 @@
+package org.example.bigo;
+
+public class BigO {
+}
---
+++ src/main/java/org/example/sorting/SortingUtility.java
@@ -0,0 +1,27 @@
+package org.example.sorting;
+
+public class SortingUtility {
+
+    private static <T extends Comparable<T>> void swap(T[] data, int index1, int index2) {
+
+        T temp = data[index1];
+        data[index1] = data[index2];
+        data[index2] = temp;
+    }
+
+    public static void mergeSort(Integer[] copy5) {
+    }
+
+    public static void quickSort(Integer[] copy4) {
+    }
+
+    public static void bubbleSort(Integer[] copy3) {
+    }
+
+    public static void insertionSort(Integer[] copy2) {
+    }
+
+    public static void selectionSort(Integer[] copy1) {
+    }
+}
+
